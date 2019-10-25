00603b1ccd0085996df27a73353ac35a525f77f0
jvmops <jvmops@gmail.com>
2019-10-25 23:56:18 +0200

    spring-app generated from initializer

---
+++ src/main/java/com/jvmops/gumtree/scrapper/GumtreeScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.jvmops.gumtree.scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumtreeScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumtreeScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jvmops/gumtree/scrapper/GumtreeScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvmops.gumtree.scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GumtreeScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
