7d6b7a13875617cbbb0af6f1636fadd6bae69234
jvmops <jvmops@gmail.com>
2020-03-03 03:00:22 +0100

    properties cleaned up

--- src/main/java/com/jvmops/gumtree/Main.java
+++ src/main/java/com/jvmops/gumtree/Main.java
@@ -1,14 +1,11 @@
 package com.jvmops.gumtree;
 
-import com.jvmops.gumtree.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
-@EnableConfigurationProperties({ApplicationProperties.class})
 public class Main {
 	@SuppressWarnings("squid:S4823")
 	public static void main(String[] args) {
--- src/main/java/com/jvmops/gumtree/config/ApplicationProperties.java
+++
@@ -1,8 +0,0 @@
-package com.jvmops.gumtree.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-// for reference
-@ConfigurationProperties(prefix = "gumtree.scrapper")
-public class ApplicationProperties {
-}
