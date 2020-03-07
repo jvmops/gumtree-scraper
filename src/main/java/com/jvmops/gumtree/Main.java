db5de6e550e2d4a7854953830652a78cbb882381
jvmops <jvmops@gmail.com>
2020-03-07 19:32:58 +0100

    #app :: mongo config got it's own class

--- src/main/java/com/jvmops/gumtree/Main.java
+++ src/main/java/com/jvmops/gumtree/Main.java
@@ -2,11 +2,10 @@ package com.jvmops.gumtree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableMongoAuditing
 public class Main {
+	// sonar stuff
 	@SuppressWarnings("squid:S4823")
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
