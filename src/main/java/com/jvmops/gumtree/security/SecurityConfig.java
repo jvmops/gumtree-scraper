5897d10b22f3dad3a47988e6a2f6676907327a80
jvmops <jvmops@gmail.com>
2020-03-07 05:26:57 +0100

    #web #security :: navigations are now displayed based on isAuthenticated()
    ugly `templateEngine.addDialect(..)` hack with `@PostConstruct`

--- src/main/java/com/jvmops/gumtree/security/SecurityConfig.java
+++ src/main/java/com/jvmops/gumtree/security/SecurityConfig.java
@@ -8,9 +8,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    TemplateEngine templateEngine;
+
     @Autowired
     private JvmopsUserRepository jvmopsUserRepository;
 
@@ -30,6 +38,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().formLogin()
                     .loginPage("/login")
                     .permitAll();
+    }
+
+    // TODO: very very ugly hack - try think of something better
+    @PostConstruct
+    void addDialect() {
+        templateEngine.addDialect(new SpringSecurityDialect());
     }
 
     @Profile("web")
