84d85bea098e3f6672084f2acafeac74db5c2082
jvmops <jvmops@gmail.com>
2020-05-03 23:36:06 +0200

    #scrapper :: pagination url template fixed

--- src/main/java/com/jvmops/gumtree/scrapper/HtmlProvider.java
+++ src/main/java/com/jvmops/gumtree/scrapper/HtmlProvider.java
@@ -12,8 +12,8 @@ import static org.springframework.util.StringUtils.capitalize;
 @Slf4j
 class HtmlProvider {
     public static final String GUMTREE_URL = "https://www.gumtree.pl";
-    private static final String URL_TEMPLATE = "%s/s-mieszkania-i-domy-do-wynajecia/%s/v1c9008l3200114q0p1";
-    private static final String URL_TEMPLATE_PAGINATED = "%s/s-mieszkania-i-domy-do-wynajecia/%s/page-%s/v1c9008l3200114q0p1";
+    private static final String URL_TEMPLATE = "%s/s-mieszkania-i-domy-do-wynajecia/%s/v1c9008l3200114p%s";
+    private static final String URL_TEMPLATE_PAGINATED = "%s/s-mieszkania-i-domy-do-wynajecia/%s/page-%s/v1c9008l3200114p%s";
 
     String adListing(String city, int pageNumber) {
         String url = getUrl(city, pageNumber);
@@ -28,8 +28,8 @@ class HtmlProvider {
     private String getUrl(String city, int pageNumber) {
         // just for lolz :)
         String url =  switch (pageNumber) {
-            case 1 -> String.format(URL_TEMPLATE, GUMTREE_URL, city);
-            default -> String.format(URL_TEMPLATE_PAGINATED, GUMTREE_URL, city, pageNumber);
+            case 1 -> String.format(URL_TEMPLATE, GUMTREE_URL, city, pageNumber);
+            default -> String.format(URL_TEMPLATE_PAGINATED, GUMTREE_URL, city, pageNumber, pageNumber);
         };
         return url;
     }
