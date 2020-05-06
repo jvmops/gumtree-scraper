cdbe354fcb95ca297f3f1b4cfcfb35980814393c
jvmops <jvmops@gmail.com>
2020-05-06 03:27:47 +0200

    #report :: subscription unsubscribe url now redirects to form

--- src/main/java/com/jvmops/gumtree/report/TemplateProcessor.java
+++ src/main/java/com/jvmops/gumtree/report/TemplateProcessor.java
@@ -21,6 +21,7 @@ public class TemplateProcessor {
 
     String initialEmail(ApartmentReport apartmentReport, String email) {
         Context context = initializeContext(apartmentReport);
+        context.setVariable("baseUrl", ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString());
         context.setVariable("email", encodeForUrl(email));
         return templateEngine.process("email/initial.html", context);
     }
@@ -34,7 +35,6 @@ public class TemplateProcessor {
         Context context = new Context(Locale.ENGLISH);
         context.setVariable("city", apartmentReport.getCity().getName());
         context.setVariable("report", apartmentReport);
-        context.setVariable("baseUrl", ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString());
         context.setVariable("cityForUrl", encodeForUrl(apartmentReport.getCity().getName()));
         return context;
     }
